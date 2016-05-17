c410cc3b4b43d7bbcde66db18e9dd6e028aed00a
Bibigon1976 <kranin@bittorrent.com>
2016-05-17 00:54:43 -0700

    auto commit

--- baseTest.py
+++ baseTest.py
@@ -1,13 +1,27 @@
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 import unittest
+import os,sys,platform
+
 
 class BaseTest(unittest.TestCase):
+    path=os.getcwd()
+    os_name=platform.system()
+
     @classmethod
     def setUpClass(cls):
-        cls.chromedriver_path = "./supply/drivers/chromedriver"
         cls.options = Options()
-        cls.options.binary_location = '../node_modules/electron-prebuilt/dist/Electron.app/Contents/MacOS/Electron'
+        if cls.os_name=='Windows':
+            cls.chromedriver_path = "./supply/drivers/chromedriver.exe"
+            cls.options.binary_location = '../node_modules/electron-prebuilt/dist/electron.exe'
+
+        elif cls.os_name=='Darvin':
+            cls.chromedriver_path = "./supply/drivers/chromedriver"
+            cls.options.binary_location = '../node_modules/electron-prebuilt/dist/Electron.app/Contents/MacOS/Electron'
+
+        else:
+            print('Unknown OS') #todo  -  implement logging
+
         cls.options.add_argument("app=../main.js")
         cls.options.add_argument('no-sandbox')     #todo: do we need it ?
         cls.driver = webdriver.Chrome(executable_path=cls.chromedriver_path, chrome_options=cls.options)
